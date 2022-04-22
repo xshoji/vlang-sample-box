c417ff50cadaa8a372483b19f7516a469c28cd3d
xshoji <xshoji@users.noreply.github.com>
2022-04-22 21:57:36 +0900

    Update

--- defer/main.v
+++ defer/main.v
@@ -20,12 +20,14 @@ fn main() {
 	mut logger := get_logger()
 	logger.info('Start')
 
+	// SIGINT ( Ctrl + C ) handling block
 	// > v/signal.v at 36ec47cd203a1f119da74b868ceb92f6da7ea930 · vlang/v
 	// > https://github.com/vlang/v/blob/36ec47cd203a1f119da74b868ceb92f6da7ea930/vlib/os/signal.v#L43
 	os.signal_opt(os.Signal.int, fn (s os.Signal) {
 		defer_func()
 		exit(1)
 	}) ?
+	// defer block
 	defer {
 		defer_func()
 	}
